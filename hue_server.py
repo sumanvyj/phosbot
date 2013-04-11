5f50a1a504fabc3392ea2aa559a141502d3c9e88
Suman Jandhyala <sumanvyj@gmail.com>
2013-04-10 18:39:39 -0700

    Add python lib to cmake

--- hue_server.py
+++ hue_server.py
@@ -1,10 +1,10 @@
-from cxx import audio;
-from time import clock;
+from cxx import audio
+from time import clock
 
 # Set up an OpenAL context and stuff
 audio.initialize()
 
-audio.play_sound("sample_audio/calm1.ogg");
+audio.play_sound("sample_audio/calm1.ogg")
 
 while clock() < 30:
   audio.update()
