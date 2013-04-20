3c905b9823279205a04b3fd2bbefe917753272b5
Suman Jandhyala <sumanvyj@gmail.com>
2013-04-19 18:02:00 -0700

    Add initial twitter connection code

--- hue_server.py
+++ hue_server.py
@@ -1,5 +1,8 @@
 from cxx import audio
 from time import clock
+from twitter import *
+import sys
+import config
 
 # Set up an OpenAL context and stuff
 audio.initialize()
@@ -8,6 +11,19 @@ audio.play_sound("sample_audio/calm1.ogg");
 
 while clock() < 3:
   audio.update()
+
+"""
+auth = OAuth(
+    config.OAUTH_TOKEN, config.OAUTH_SECRET,
+    config.CONSUMER_KEY, config.CONSUMER_SECRET
+)
+"""
+auth = UserPassAuth(config.USERNAME, config.PASSWORD)
+twitter_stream = TwitterStream(auth=auth)
+stream = twitter_stream.statuses.sample()
+
+for tweet in stream:
+    sys.stdout.write('%r\n' % tweet[u'text'])
 
 # TODO implement the server part...
 
