cba44f218ea585219a3769aec2d2919a4f90d68e
David Mailhot <djmailhot@gmail.com>
2013-04-19 23:54:32 -0700

    commandparser power boolean, color to rgb

--- speech/commandparser.py
+++ speech/commandparser.py
@@ -15,10 +15,11 @@ import webcolors
 def _enum(**enums):
     return type('Enum', (), enums)
 
+Power = _enum(ON='on', OFF='off')
 # absolute brightness
-Power = _enum(ON='on', OFF='off', BRIGHT='bright', DIM='dim')
+SetLight = _enum(BRIGHT='bright', DIM='dim', HALF='half', FULL='full')
 # relative brightness
-Bright = _enum(UP='up', DOWN='down', BRIGHTER='brighter', DIMMER='dimmer')
+ChangeLight = _enum(UP='up', DOWN='down', BRIGHTER='brighter', DIMMER='dimmer')
 # song volume
 Volume = _enum(LOUD='loud', SOFT='soft', HIGH='high', LOW='low')
 
@@ -26,9 +27,12 @@ Volume = _enum(LOUD='loud', SOFT='soft', HIGH='high', LOW='low')
 class StateChange(object):
     '''
     StateChange.power
+        boolean on / off {true/false}, true being on
+        if None, no change of state.
+    StateChange.setlight
         absolute brightness setting {a percentage brightness set}
         if None, no change of state.
-    StateChange.brightness
+    StateChange.changelight
         relative brightness change.  {a percentage brightness change}
         if None, no change of state.
     StateChange.names
@@ -45,11 +49,14 @@ class StateChange(object):
         if None, play at normal volume
     '''
 
-    __slots__ = ('power', 'brightness', 'names', 'color', 'song', 'volume')
+    __slots__ = (
+        'power', 'setlight', 'changelight', 'names', 'color', 'song', 'volume'
+    )
 
     def __init__(self, **kwargs):
         self.power = kwargs.get('power')
-        self.brightness = kwargs.get('brightness')
+        self.setlight = kwargs.get('setlight')
+        self.changelight = kwargs.get('changelight')
         self.names = kwargs.get('names')
         self.color = kwargs.get('color')
         self.song = kwargs.get('song')
@@ -59,10 +66,10 @@ class StateChange(object):
         return self.__str__()
 
     def __str__(self):
-        return '%s(power=%r, brightness=%r, target=%r, color=%r, song=%r, volume=%r)' % (
+        return '%s(power=%r, setlight=%r, changelight=%r, target=%r, color=%r, song=%r, volume=%r)' % (
             self.__class__.__name__,
-            self.power, self.brightness, self.names,
-            self.color, self.song, self.volume
+            self.power, self.setlight, self.changelight,
+            self.names, self.color, self.song, self.volume
         )
 
 
@@ -83,34 +90,44 @@ def _hasextra(cmd):
 
 def parse_power(cmd, state):
     val = 0
+    if _match(cmd, Power.ON):
+        val = 100
+    if _match(cmd, Power.OFF):
+        val = 0
+    state.power = val
+    return (cmd, state)
+
+
+def parse_setlight(cmd, state):
+    val = 0
     extra = _hasextra(cmd)
 
     # no elifs because someone may say "Turn on lights to dim"
     # more of a key-word search
-    if _match(cmd, Power.ON) or _match(cmd, Power.BRIGHT):
+    if _match(cmd, SetLight.FULL) or _match(cmd, SetLight.BRIGHT):
         val = 100
-    if _match(cmd, Power.DIM):
+    if _match(cmd, SetLight.HALF):
+        val = 50
+    if _match(cmd, SetLight.DIM):
         val = 30
         val /= 2 if extra else 1
-    if _match(cmd, Power.OFF):
-        val = 0
 
-    state.power = val
+    state.setlight = val
     return (cmd, state)
 
-def parse_brightness(cmd, state):
+def parse_changelight(cmd, state):
     val = 0
     extra = _hasextra(cmd)
 
     # no elifs because someone may say "Turn down lights to dimmer"
     # more of a key-word search
-    if _match(cmd, Bright.UP) or _match(cmd, Bright.BRIGHTER):
+    if _match(cmd, ChangeLight.UP) or _match(cmd, ChangeLight.BRIGHTER):
         val = 40
-    if _match(cmd, Bright.DOWN) or _match(cmd, Bright.DIMMER):
+    if _match(cmd, ChangeLight.DOWN) or _match(cmd, ChangeLight.DIMMER):
         val = -40
 
     val *= 2 if extra else 1
-    state.brightness = val
+    state.changelight = val
     return (cmd, state)
 
 def parse_names(cmd, state, names):
@@ -128,12 +145,15 @@ def parse_names(cmd, state, names):
 def parse_color(cmd, state):
     for word in cmd:
         try:
-            state.color = webcolors.name_to_hex(word, spec='css3')
+            state.color = webcolors.name_to_rgb(word)
             cmd.remove(word)
             break
         except ValueError:
             continue
     return (cmd, state)
+
+#def parse_song(cmd, state):
+
 
 
 def parse_eastereggs(cmd, state):
@@ -157,7 +177,8 @@ def process_command(cmd, names=list()):
     _match(cmd, 'turn')
 
     cmd, state = parse_power(cmd, state)
-    cmd, state = parse_brightness(cmd, state)
+    cmd, state = parse_setlight(cmd, state)
+    cmd, state = parse_changelight(cmd, state)
     cmd, state = parse_names(cmd, state, names)
     cmd, state = parse_color(cmd, state)
     cmd, state = parse_eastereggs(cmd, state)
