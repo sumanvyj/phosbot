6de68ccf88b606cd9e8391ee616f4bae1380ad6c
Wilson Lee <kourge@gmail.com>
2013-04-20 00:26:55 -0700

    Make walter smarter

--- walter.py
+++ walter.py
@@ -22,6 +22,11 @@ class Walter(object):
 
         return cls._bridge
 
+    MAX_BRIGHTNESS = 254
+    MAX_COLOR = 255.0
+    MAX_HUE = 65535
+    MAX_SATURATION = 254
+
     @classmethod
     def control_lights(cls, phrase):
         if cls._username_re.search(phrase) is None:
@@ -33,8 +38,28 @@ class Walter(object):
         state = speech.commandparser.process_command(phrase)
 
         names = state.names if state.names else [x.name for x in b.lights]
-        command = {'on': bool(state.power)}
-        b.set_light(names, command)
+        command = {}
+
+        if state.power is not None:
+            command['on'] = state.power
+
+        if state.setlight is not None:
+            command['brightness'] = int(cls.MAX_BRIGHTNESS * (state.setlight / 100.0))
+
+        if state.color is not None:
+            r, g, b = state.color
+            r, g, b = r / MAX_COLOR, g / MAX_COLOR, b / MAX_COLOR
+            h, s, v = colorsys.rgb_to_hsv(r, g, b)
+            command['hue'] = int(MAX_HUE * h)
+            command['sat'] = int(MAX_SATURATION * s)
+
+
+        if state.changelight is not None:
+            for light in (for x in b.lights if x.name in names):
+                factor = (100 + state.changelight) / 100.0
+                light.brightness = int(light.brightness * factor)
+        else:
+            b.set_light(names, command)
 
         return state
 
